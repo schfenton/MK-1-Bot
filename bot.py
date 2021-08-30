3c4df3a395c61c9152d377a2b3453a0b39e58b9b
schfenton <schfenton@gmail.com>
2021-08-30 02:04:55 -0700

    Added very basic queue feature. Todo: Mark voice channel for invite, delete queue message after amount of time, argument for user limit.

--- bot.py
+++ bot.py
@@ -25,6 +25,9 @@ except FileNotFoundError:
 with open('pokemon.json', 'r') as f:
     pokemon_list = json.load(f)
 
+queueList = {}
+voiceChannel = 737612005242044447
+
 
 @bot.event
 async def on_ready():
@@ -41,7 +44,25 @@ async def on_message(message):
     for emote_id in emote_ids:
         emoteList.add(emote_id, message.guild.id, message.author)
     print(emote_ids)
-    return None
+
+
+@bot.event
+async def on_reaction_add(reaction, r_user):
+    if reaction.message.id in queueList:
+        if reaction.count >= queueList[reaction.message.id]:
+            invite = await reaction.message.guild.get_channel(voiceChannel).create_invite(max_age=300)
+            async for user in reaction.users():
+                if user != bot.user:
+                    await user.send("Time to join! " + invite.url)
+            await reaction.message.channel.send("Queue ended, join the voice channel.")
+            await reaction.message.delete()
+            del queueList[reaction.message.id]
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        await ctx.send("God damn, you stupid.")
 
 
 @bot.command(name='hello')
@@ -71,6 +92,14 @@ async def get_pokemon_name(ctx, *, arg):
         elif 65 <= ord(c) <= 90:  # if uppercase
             total += ord(c) - 64
     await ctx.send(pokemon_list[total])
+
+
+@bot.command(name='newqueue')
+async def new_event_queue(ctx):
+    msg = await ctx.send(embed=Embed(title="Queue started", description="Click the check mark to be notified when 2 "
+                                                                        "people are ready!"))
+    queueList[msg.id] = 2
+    await msg.add_reaction("✅")
 
 
 @bot.command(name='kill')
